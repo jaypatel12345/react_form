685881527cba73102f6c3bed09406d76f8aecac0
jerrybhai <pateljai9442@gmail.com>
2020-07-08 12:56:14 +0530

    exploring git

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,6 +4,7 @@ import React from 'react'
 const About = (props) => {
     // console.log(props.location.state[0])
     // let { Name,Password,Address,Mobile } = props.location.state[0];
+  console.log('exploring git')
     return (
       <div>
         <h1>This is About page</h1>
